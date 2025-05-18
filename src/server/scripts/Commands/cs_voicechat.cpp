39efea98168d863a3aad880e784a67e184df3970
heyitsbench <benjymansy123@gmail.com>
2025-05-18 17:46:12 -0400

    Headers, log.

--- src/server/scripts/Commands/cs_voicechat.cpp
+++ src/server/scripts/Commands/cs_voicechat.cpp
@@ -15,13 +15,6 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* ScriptData
-Name: voicechat_commandscript
-%Complete: 100
-Comment: All bf related commands
-Category: commandscripts
-EndScriptData */
-
 #include "VoiceChatMgr.h"
 #include "VoiceChatChannel.h"
 #include "Chat.h"
