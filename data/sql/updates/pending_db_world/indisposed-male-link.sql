84a262965838eccb718906aeae840a2534a68502
heyitsbench <benjymansy123@gmail.com>
2026-05-07 06:53:51 -0400

    Init.

---
+++ data/sql/updates/pending_db_world/indisposed-male-link.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = -48323;
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
+(-48323, 48330, 0, 'On Indisposed Expiring - Cast Create Amberseeds');
