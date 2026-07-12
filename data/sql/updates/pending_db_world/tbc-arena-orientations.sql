11dd65b5ae144ba4151eb10f28c1e6f4d76430c0
heyitsbench <benjymansy123@gmail.com>
2026-07-12 18:34:51 -0400

    Init.

---
+++ data/sql/updates/pending_db_world/tbc-arena-orientations.sql
@@ -0,0 +1,3 @@
+UPDATE `battleground_template` SET `AllianceStartO` = 5.2359877, `HordeStartO` = 2.0943952 WHERE `ID` = 4;
+UPDATE `battleground_template` SET `AllianceStartO` = 3.1415927, `HordeStartO` = 0         WHERE `ID` = 5;
+UPDATE `battleground_template` SET `AllianceStartO` = 0.7853982, `HordeStartO` = 2.3561945 WHERE `ID` = 8;
